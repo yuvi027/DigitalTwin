e8f2ca61ce88521ab035aea9ec0269162da9dce6
yuvi027 <36572544+yuvi027@users.noreply.github.com>
2025-01-04 11:06:20 +0000

    updated exam only sh

--- v7/install-flows-exam-mode-only.sh
+++ v7/install-flows-exam-mode-only.sh
@@ -5,3 +5,14 @@ echo "Setting up exam mode configuration"
 # Drop all student traffic
 sudo ovs-ofctl add-flow s1 ip,priority=1,nw_src=10.0.0.1,idle_timeout=0,actions=drop
 sudo ovs-ofctl add-flow s4 ip,priority=1,nw_src=10.0.0.2,idle_timeout=0,actions=drop
+
+# I'm not sure if we need to add the normal rules for the researchers' communication
+# # Adding queue flow rules
+# sudo ovs-ofctl add-flow s3 ip,priority=1,in_port=1,idle_timeout=0,actions=output:2
+# sudo ovs-ofctl add-flow s3 ip,priority=1,in_port=2,idle_timeout=0,actions=output:1
+
+# sudo ovs-ofctl add-flow s4 ip,priority=1,nw_src=10.0.0.3,nw_dst=10.0.0.4,idle_timeout=0,actions=output:4
+# sudo ovs-ofctl add-flow s4 ip,priority=1,nw_src=10.0.0.4,nw_dst=10.0.0.3,idle_timeout=0,actions=output:2
+
+# sudo ovs-ofctl add-flow s1 ip,priority=1,nw_src=10.0.0.3,nw_dst=10.0.0.4,idle_timeout=0,actions=output:2
+# sudo ovs-ofctl add-flow s1 ip,priority=1,nw_src=10.0.0.4,nw_dst=10.0.0.3,idle_timeout=0,actions=output:4
