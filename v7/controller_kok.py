e97c329372f67af9e437701d37908278c35a1835
yuvi027 <36572544+yuvi027@users.noreply.github.com>
2025-01-08 09:11:54 +0100

    Update controller_kok.py

--- v7/controller_kok.py
+++ v7/controller_kok.py
@@ -1,3 +1,5 @@
+# NOT WORKING!!! USE OTHER CONTROLLER
+
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
